f32aac0a39646c12ab032fdb485551bf57877f88
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-29 10:14:27 -0400

    switched to class based views

--- noteclick/main/api/views.py
+++ noteclick/main/api/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from .serializers import GameSerializer
 from ..models import Game
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(["GET"])
-def get_game_data(req):
-    game = get_object_or_404(Game, user=req.user)
-    ser = GameSerializer(game)
-    return Response(ser.data)
+class GetGameData(APIView):
+    http_method_names = ["GET"]
+
+    def get(self, req):
+        game = get_object_or_404(Game, user=req.user)
+        ser = GameSerializer(game)
+        return Response(ser.data)
