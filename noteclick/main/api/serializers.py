2d55a0cda5e6a839238dd1dd69e64b110497a87d
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-04 11:14:23 -0400

    created model serializer

---
+++ noteclick/main/api/serializers.py
@@ -0,0 +1,7 @@
+from ..models import Game
+from rest_framework.serializers import ModelSerializer
+
+class GameSerializer(ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('points', 'cps')
