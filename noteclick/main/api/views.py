3874ab31192c8ced8a51c30c1b6b74df88f419be
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-04 11:25:59 -0400

    created new view

--- noteclick/main/api/views.py
+++ noteclick/main/api/views.py
@@ -1,11 +1,18 @@
 from .serializers import GameSerializer
 from ..models import Game
 from rest_framework.response import Response
+
 from rest_framework.views import APIView
-from django.shortcuts import get_object_or_404
 
 
 class GetGameDataView(APIView):
     def get(self, req):
         if req.user.is_authenticated:
-            game = get_object_or_404(Game, user=req.user)
+            try:
+                game : Game = Game.objects.get(user=req.user)
+            except:
+                game = Game.objects.create(user=req.user)
+
+            ser = GameSerializer(game)
+            return Response(ser.data)
+                
