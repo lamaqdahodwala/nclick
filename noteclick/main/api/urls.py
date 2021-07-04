7b36b38a150991392148f2bca36b4d2647ea239e
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-04 16:00:07 -0400

    created new url routes

--- noteclick/main/api/urls.py
+++ noteclick/main/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("get_game", views.GetGameDataView.as_view())]
+urlpatterns = [
+    path("get_game", views.GetGameDataView.as_view()),
+    path('save', views.SaveGameView.as_view())
+]
