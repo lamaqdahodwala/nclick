ba20854896cf9c2a85696589f5ec279db3274ec3
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-04 16:15:55 -0400

    incorporated user authentication

--- noteclick/noteclick/settings.py
+++ noteclick/noteclick/settings.py
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [str(BASE_DIR.parent / "svelte" / "public" / "build")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
