072e31714986a40b4b3fb7dbabd2798b8f161ea7
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-04 11:39:03 -0400

    added files in base url routing

--- noteclick/noteclick/urls.py
+++ noteclick/noteclick/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path('api/v1/', include('main.api.urls'))
+    path('api/v1/', include('main.api.urls')),
+    path('account/', include('accounts.urls'))
 ]
